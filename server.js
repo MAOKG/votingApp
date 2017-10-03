2925e0ea2bc7cd42f3c46f1ec25bb64cdc8951ee
Maosen <maosen1993@gmail.com>
2017-10-03 09:36:54 -0400

    Add SSR

--- server.js
+++ server.js
@@ -14,25 +14,60 @@ require('./services/passport');
 const authRoutes = require('./routes/authRoutes');
 const pollRoutes = require('./routes/pollRoutes');
 
+/* =======  Sever side rendering ========== */
+const React = require('react');
+const ReactDOMServer = require('react-dom/server');
+const ReactRouter = require('react-router-dom');
+const _ = require('lodash');
+const fs = require('fs');
+const compression = require('compression');
+const App = require('./client/js/App').default;
+
+const StaticRouter = ReactRouter.StaticRouter;
+const baseTemplate = fs.readFileSync('./index.html');
+const template = _.template(baseTemplate);
+/* ======================================== */
+
 const PORT = process.env.PORT || 5000;
-const app = express();
+const server = express();
 
 mongoose.connect(keys.mongoURI);
 
-app.use(
+server.use(
   cookieSession({
     maxAge: 10 * 60 * 1000,
     keys: [keys.cookieKey]
   })
 );
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(flash());
-app.use(passport.initialize());
-app.use(passport.session());
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(flash());
+server.use(passport.initialize());
+server.use(passport.session());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/polls', pollRoutes);
+server.use('/api/auth', authRoutes);
+server.use('/api/polls', pollRoutes);
 
-app.listen(PORT, () => {
+/* =======  Sever side rendering ========== */
+if (process.env.NODE_ENV === 'production') {
+  server.use(compression());
+  server.use('/client/public', express.static('./client/public'));
+
+  server.get('*', (req, res) => {
+    console.log(req.url);
+    const context = {};
+    const body = ReactDOMServer.renderToString(
+      React.createElement(StaticRouter, { location: req.url, context }, React.createElement(App))
+    );
+
+    if (context.url) {
+      res.redirect(context.url);
+    }
+
+    res.write(template({ body }));
+    res.end();
+  });
+}
+/* ======================================== */
+server.listen(PORT, () => {
   console.log('Server is running!');
 });
